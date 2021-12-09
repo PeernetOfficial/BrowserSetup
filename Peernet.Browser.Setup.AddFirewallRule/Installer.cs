67f6b07c1a39b69484a1436bd0a20d96eadf10e6
Krzysztof Skubała <Krzysztof.Skubala@cshark.com>
2021-12-09 12:43:46 +0100

    Fix firewall rule creation issue related to %cd% iconsistancy with admin rights

--- Peernet.Browser.Setup.AddFirewallRule/Installer.cs
+++ Peernet.Browser.Setup.AddFirewallRule/Installer.cs
@@ -10,6 +10,7 @@ namespace Peernet.Browser.Setup.AddFirewallRule
     public partial class Installer : System.Configuration.Install.Installer
     {
         private const string FirewallRuleName = "Peernet Browser";
+        private const string BatchFileName = "Firewall allow.cmd";
 
         public Installer()
             : base()
@@ -17,49 +18,49 @@ namespace Peernet.Browser.Setup.AddFirewallRule
             Committed += new InstallEventHandler(MyInstaller_Committed);
         }
 
-        private void MyInstaller_Committed(object sender, InstallEventArgs e)
+        private static void CreateFile(string directory, string fileName, string command)
         {
-            string netshCommandArguments = $"advfirewall firewall add rule name=\"{FirewallRuleName}\" dir=in program=\"%cd%\\Backend.exe\" profile=any action=allow";
-            CreateBatchFile(GetTargetDirectory(), "netsh " + netshCommandArguments);
-            ExecuteWithArgs("netsh.exe", netshCommandArguments);
-        }
-
-        private void ExecuteWithArgs(string processName, string args)
-        {
-            var process = new Process();
-            var processStartInfo = new ProcessStartInfo();
-            processStartInfo.WindowStyle = ProcessWindowStyle.Hidden;
-            processStartInfo.FileName = processName;
-            processStartInfo.Arguments = args;
-            processStartInfo.Verb = "runas";
-
-            process.StartInfo = processStartInfo;
-            try
+            using (var streamWriter = File.CreateText($"{directory}/{fileName}"))
             {
-                process.Start();
-            }
-            catch (Exception e)
-            {
-                CreateFile(GetTargetDirectory(), "firewallnotset", e.Message + "\n" + e.StackTrace);
+                streamWriter.WriteLine(command);
             }
         }
 
         private void CreateBatchFile(string directory, string command)
         {
-            CreateFile(directory, "Firewall allow", command);
+            CreateFile(directory, BatchFileName, command);
         }
 
-        private void CreateFile(string directory, string fileName, string command)
+        private void Execute(string processName)
         {
-            using (var streamWriter = File.CreateText($"{directory}/{fileName}.cmd"))
+            var process = new Process();
+            var processStartInfo = new ProcessStartInfo();
+            processStartInfo.WindowStyle = ProcessWindowStyle.Normal;
+            processStartInfo.FileName = processName;
+            processStartInfo.Verb = "runas";
+            processStartInfo.WorkingDirectory = GetTargetDirectory();
+            process.StartInfo = processStartInfo;
+
+            try
             {
-                streamWriter.WriteLine(command);
+                process.Start();
+            }
+            catch (Exception)
+            {
+                CreateFile(GetTargetDirectory(), "firewallnotset", string.Empty);
             }
         }
 
         private string GetTargetDirectory()
         {
             return Context.Parameters["targetdir"].Replace(@"\\", @"\");
+        }
+
+        private void MyInstaller_Committed(object sender, InstallEventArgs e)
+        {
+            string netshCommand = $"netsh advfirewall firewall add rule name=\"{FirewallRuleName}\" dir=in program=\"%~dp0\\Backend.exe\" profile=any action=allow";
+            CreateBatchFile(GetTargetDirectory(), netshCommand);
+            Execute(BatchFileName);
         }
     }
 }
